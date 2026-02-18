e1ac336d50ce34ce8e98624f66b05d1eed7937f4
Ben hmida Yassmine <yassminebenhmida671@gmail.com>
2026-02-18 01:28:42 +0100

    3 change

--- js/main1.js
+++ js/main1.js
@@ -41,7 +41,7 @@ function clickReservation() {
     }
 }
 
-fetch("data.json")
+/*fetch("data.json")
   .then(response => response.json())
   .then(data => {
 
@@ -80,7 +80,49 @@ fetch("data.json")
 
     });
 
+  });*/
+  fetch("data.json")
+  .then(response => response.json())
+  .then(data => {
+
+    const container = document.getElementById("forfaitContainer");
+
+    data.forfaits.forEach(item => {
+
+      let stars = "";
+      for(let i = 0; i < item.rating; i++){
+        stars += '<i class="fas fa-star"></i>';
+      }
+
+      const box = `
+        <div class="box">
+          <img src="${item.image}" alt="">
+          <div class="content">
+            <h3><i class="fas fa-map-marker-alt"></i> ${item.name}</h3>
+
+            <p>${item.desc}</p>
+
+            <div class="stars">
+              ${stars}
+            </div>
+
+            <div class="price">
+              $${item.price} <span>$${item.oldPrice}</span>
+            </div>
+
+            <br>
+            <a href="#reserver" class="btn">reserver</a>
+            <a href="#" class="btn1">detail</a>
+          </div>
+        </div>
+      `;
+
+      container.innerHTML += box;
+
+    });
+
   });
+
 
   
 
